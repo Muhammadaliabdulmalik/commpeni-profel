17115f7babd5d2323e861a5ab5f1f1999e2ce413
Muhammadaliabdulmalik <alimalik1403@gmail.com>
2021-12-20 14:14:20 +0700

    commpeni profeli

---
+++ src/components/A.vue
@@ -0,0 +1,282 @@
+<template>
+  <div>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">tugas 1</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">about</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">galeri</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">content</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">testimoni</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <!-- navbar -->
+    <!-- jumboteron -->
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <div class="row">
+          <div class="col text-center">
+            <h1 class="nav-link">COMPANY PROFILE</h1>
+          </div>
+        </div>
+        <div class="about">
+          <div class="row box-about mb-5 pt-5">
+            <div class="col box-about-col">
+              <h1 class="nav-link">ABOUT</h1>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md box-about-col">
+              <img src="../assets/image-about.jpg" alt="" />
+            </div>
+            <div class="col-md box-about d-flex align-items-center">
+              <p class="a">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Excepturi optio voluptates necessitatibus, tempora accusamus
+                molestiae nobis natus, ut similique reiciendis asperiores id
+                modi soluta! Itaque vel libero doloremque aut aspernatur
+                corporis aperiam consectetur quidem, fugiat deleniti quasi alias
+                distinctio, facere minus? Fugiat quidem, qui voluptatibus quam
+                voluptates expedita possimus eos aspernatur tempora sit atque
+                placeat nulla cumque veritatis repellendus dolor recusandae
+                quas, perferendis praesentium deleniti sapiente consequatur
+                ratione et rerum! Velit eum pariatur ex at qui animi molestiae
+                commodi beatae?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- akhir jumboteron -->
+    <section id="galeri" class="galeri">
+      <div class="container">
+        <div class="row mb-5 pt-5">
+          <div class="col-md text-center">
+            <h1 class="a">galeri</h1>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md">
+            <div class="card">
+              <img
+                class="card-img-top"
+                src="../assets/bady-abbas-jOmBUCtflWA-unsplash.jpg"
+                alt="Card image cap"
+              />
+              <div class="card-body">
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="card">
+              <img
+                class="card-img-top"
+                src="../assets/christopher-gower-m_HRfLhgABo-unsplash.jpg"
+                alt="Card image cap"
+              />
+              <div class="card-body">
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="card">
+              <img
+                class="card-img-top"
+                src="../assets/clement-helardot-95YRwf6CNw8-unsplash.jpg"
+                alt="Card image cap"
+              />
+              <div class="card-body">
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md">
+            <div class="card">
+              <img
+                class="card-img-top"
+                src="../assets/jay-zhang-ycZjz2Uw6VY-unsplash.jpg"
+                alt="Card image cap"
+              />
+              <div class="card-body">
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md">
+            <img
+              class="card-img-top"
+              src="../assets/kari-shea-1SAnrIxw5OY-unsplash.jpg"
+              alt="0"
+            />
+            <div class="card-body">
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+          <div class="col-md">
+            <img
+              class="card-img-top"
+              src="../assets/pedro-costa-aXY5doQNZTc-unsplash.jpg"
+              alt="Card image cap"
+            />
+            <div class="card-body">
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="row mb-5 pt-5">
+          <div class="col-md">
+            <div class="col text-center">
+              <h1 class="a">testimoni</h1>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md">
+            <img
+              class="card-img-top rounded-circle"
+              src="../assets/testi1.jpg"
+              alt="Card-image-to"
+            />
+            <div class="card-body">
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+          <div class="col-md">
+            <img
+              class="card-img-top rounded-circle"
+              src="../assets/testi2.jpg"
+              alt="Card-image-to"
+            />
+            <div class="card-body">
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+          <div class="col-md">
+            <img
+              class="card-img-top rounded-circle"
+              src="../assets/testi3.jpg"
+              alt="Card image cap"
+            />
+            <div class="card-body">
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="row mb-0 pt-4">
+          <div class="col-md">
+            <div class="col text-center">
+              <h1 class="a">Contact Us</h1>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md md-6 mx-auto mt-5">
+            <form action="">
+              <div class="form grup">
+                <label form="nama">useranme</label>
+                <input type="text" class="form-control" id="name" />
+              </div>
+              <div class="form grup">
+                <label form="Email">Email address</label>
+                <input
+                  type="Email"
+                  class="form-control"
+                  id="Email"
+                  aria-describedby="emailHelp"
+                />
+              </div>
+              <div class="form grup">
+                <label form="Password">Password</label>
+                <input type="Password" class="form-control" id="Password" />
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+          </div>
+        </div>
+        <footer>
+          <p class="text-center">© muhammd ali abdul malik2021</p>
+        </footer>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "A",
+};
+</script>
+
+<style scoped>
+.about img {
+  width: 100%;
+}
+.navbar-brand {
+  font-family: "Bebas Neue", cursive;
+}
+.nav-link active {
+  font-family: "Bebas Neue", cursive;
+}
+.nav-link {
+  font-family: "Bebas Neue", cursive;
+}
+.a {
+  font-family: "Anton", sans-serif;
+}
+</style>
